3c2b9f4036697c562fe5e6062e6ff6795d2d54ad
SuprajaNagamalla <supraja.nagamalla@gmail.com>
2025-11-07 22:54:01 +0530

    added secureKey file to repo

---
+++ secureKey 1.bat
@@ -0,0 +1,12 @@
+@echo off
+:: ==========================================
+:: Set MASTER_KEY as an environment variable
+:: ==========================================
+
+:: For current user only (no admin needed)
+setx MASTER_KEY "Q2m80w60dpEpj7KT517u6NjGAJHBzbSFSMN9APK3ReG6E97wZ7Vgj68g3ewaZeemF07JWpbVjZYF14cgFX9QT7Ly5dmYEyvvuCR8UzQA7ruMWCDZGWSiyfDgCiwWQQj9"
+
+echo.
+echo  MASTER_KEY has been set for your account.
+echo  Restart Command Prompt / PowerShell to use it.
+pause
